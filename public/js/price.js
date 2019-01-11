efa3d38297a393e1ee70dd04cdde434c986c5c5f
bestboom <certseek@gmail.com>
2019-01-12 00:22:48 +0500

    Update price.js

--- public/js/price.js
+++ public/js/price.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
   setInterval(function(){
     $.getJSON("https://api.coincap.io/v2/assets/tron", function(result){
-      
-        
         var price = result.data['priceUsd'];
+        var inputvalue = $('#trx_input').val();
         $('#name').html(result.data['id']);
         $('#price').html('$' + parseFloat(price).toFixed(6));
         $('#change').html('(' + (parseFloat(result.data['changePercent24Hr']).toFixed(2)) + '%)');
         $('#rank').html(result.data['rank']);
-      $('#cap').html('$' + ((result.data['marketCapUsd'])/1000000000).toFixed(2) + 'B');
-      $('#volumetoday').html('$' + ((result.data['volumeUsd24Hr'])/1000000).toFixed(2) + 'M');
-      $('#supply').html(((result.data['supply'])/1000000000).toFixed(2) + 'B TRX' );
+        $('#cap').html('$' + ((result.data['marketCapUsd'])/1000000000).toFixed(2) + 'B');
+        $('#volumetoday').html('$' + ((result.data['volumeUsd24Hr'])/1000000).toFixed(2) + 'M');
+        $('#supply').html(((result.data['supply'])/1000000000).toFixed(2) + 'B TRX' );
+        $('#usd_price').html(inputvalue * price);
     });
   }, 1000);
 });
