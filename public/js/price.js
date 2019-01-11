739f19a7439c4d6c088167f04e13f9e4bf47d72e
bestboom <certseek@gmail.com>
2019-01-11 21:19:05 +0500

    Update price.js

--- public/js/price.js
+++ public/js/price.js
@@ -8,6 +8,10 @@ $(document).ready(function(){
         $('#price').html('$' + parseFloat(price).toFixed(6));
         $('#change').html('(' + (parseFloat(result.data['changePercent24Hr']).toFixed(2)) + '%)');
         $('#rank').html(result.data['rank']);
+      $('#cap').html(result.data['marketCapUsd']);
+      $('#volumetoday').html(result.data['volumeUsd24Hr']);
+      $('#supply).html(result.data['supply']);
+      $('#maxsupply').html(result.data['maxSupply']);
     });
   }, 1000);
 });
