04bae6e0ec104bf4ec36b4ba150dbe5d739008ea
bestboom <certseek@gmail.com>
2019-01-11 14:55:09 +0500

    Update price.js

--- public/js/price.js
+++ public/js/price.js
@@ -1,1 +1,13 @@
-
+$(document).ready(function(){
+  setInterval(function(){
+    $.getJSON("https://api.coincap.io/v2/assets/tron", function(result){
+      
+        
+        var price = result.data['priceUsd'];
+        $('#name').html(result.data['id']);
+        $('#price').html('$' + parseFloat(price).toFixed(6));
+        $('#change').html('(' + (parseFloat(result.data['changePercent24Hr']).toFixed(2)) + '%)');
+        $('#rank').html(result.data['rank']);
+    });
+  }, 1000);
+});
