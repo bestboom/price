c32e9d71096420fa96197fed506c794455533c7c
bestboom <certseek@gmail.com>
2019-01-12 22:45:04 +0500

    Update price.js

--- public/js/price.js
+++ public/js/price.js
@@ -22,7 +22,7 @@ $(document).ready(function(){
         $('#cap2').html('$' + ((result.data['marketCapUsd'])/1000000000).toFixed(2) + 'B');
         $('#volumetoday2').html('$' + ((result.data['volumeUsd24Hr'])/1000000).toFixed(2) + 'M');
         $('#supply2').html(((result.data['supply'])/1000000000).toFixed(2) + 'B TRX' );
-        $("#changes:contains('-')")..removeClass('positive').addClass('negative');
+        $("#changes:contains('-')").removeClass('positive').addClass('negative');
     });
   }, 1000);
 });
