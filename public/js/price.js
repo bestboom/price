18fec70609019c14181f8fd41257f6f9c470d2e0
bestboom <certseek@gmail.com>
2019-01-12 22:05:57 +0500

    Update price.js

--- public/js/price.js
+++ public/js/price.js
@@ -25,7 +25,7 @@ $(document).ready(function(){
         //$("#changes:contains('-')").addClass('negative');
     });
             $('#changes').each(function() {
-          if (parseFloat($(this).text() < 0)) {
+          if (parseFloat($(this).text():contains('-'))) {
             $(this).addClass('negative');
              } else {
             $(this).addClass('positive');
